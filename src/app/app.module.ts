b85163474d30a2f7508eac228cf607bbf65e4fde
Geoffrey van Driessel <geoffrey.v.d@live.nl>
2017-02-27 16:37:15 +0100

    guard and routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,16 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { Http, RequestOptions } from '@angular/http';
 import { AuthHttp, AuthConfig, AUTH_PROVIDERS, provideAuth } from 'angular2-jwt';
-
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { GettersComponent } from './getters/getters.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { HomeComponent } from './home/home.component';
+
+import { AuthGuard } from './services/auth-guard.service';
+import { AuthService } from './services/auth.service';
 
 export function authHttpServiceFactory(http: Http, options: RequestOptions) {
   return new AuthHttp(new AuthConfig({
@@ -18,18 +23,37 @@ export function authHttpServiceFactory(http: Http, options: RequestOptions) {
     }), http, options);
 }
 
+const appRoutes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'dashboard', component: DashboardComponent, canActivate: [AuthGuard] },
+  { path: 'getters', component: GettersComponent, canActivate: [AuthGuard] },
+  { path: '',
+    redirectTo: '/home',
+    pathMatch: 'full' 
+  },
+  { path: '**',
+    redirectTo: '/home'
+  }
+];
+
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    GettersComponent
+    GettersComponent,
+    DashboardComponent,
+    HomeComponent
   ],
   imports: [
+    RouterModule.forRoot(appRoutes),
     BrowserModule,
     FormsModule,
     HttpModule
   ],
   providers: [
+    AuthGuard,
+    AuthService,
     {
       provide: AuthHttp,
       useFactory: authHttpServiceFactory,
